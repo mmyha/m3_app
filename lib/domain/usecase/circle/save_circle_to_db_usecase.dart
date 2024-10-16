5dde317e820911708759ae2ec4cc49f036d0bcd9
H-miura-fms <mmyha1289hm@gmail.com>
2024-10-16 13:02:15 +0900

    feat:apiから取得したサークルリストをDBに保存するユースケースを作成

---
+++ lib/domain/usecase/circle/save_circle_to_db_usecase.dart
@@ -0,0 +1,20 @@
+import 'package:m3_app/core/utils/result.dart';
+
+import '../../../infrastructure/repository/circle_repository.dart';
+
+/**
+ * APIから取得して、DBに保存
+ */
+class SaveCircleInfoToDBUseCase {
+  SaveCircleInfoToDBUseCase(this._circleRepository);
+  final CircleRepository _circleRepository;
+
+  Future<Result<void, Exception>> call() async {
+    try {
+      final result = _circleRepository.saveCircleInfoToDB();
+      return Success(result);
+    } on Exception catch (e) {
+      return Failure(e);
+    }
+  }
+}
