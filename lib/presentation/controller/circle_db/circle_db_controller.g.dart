336b2b1aedb99475c51b2d8f726251537ea4c0b5
H-miura-fms <mmyha1289hm@gmail.com>
2024-10-19 00:12:46 +0900

    feat:サークルDB関連コントローラー作成

---
+++ lib/presentation/controller/circle_db/circle_db_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'circle_db_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$circleDBControllerHash() =>
+    r'c6f69df2a52cdf8dce01f9a36043ad51bcbb18b7';
+
+/// See also [CircleDBController].
+@ProviderFor(CircleDBController)
+final circleDBControllerProvider =
+    AutoDisposeNotifierProvider<CircleDBController, Object?>.internal(
+  CircleDBController.new,
+  name: r'circleDBControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$circleDBControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CircleDBController = AutoDisposeNotifier<Object?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
