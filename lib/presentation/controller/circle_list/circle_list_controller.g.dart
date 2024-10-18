316365c29bdaedb33782735db83435c16f1cba20
H-miura-fms <mmyha1289hm@gmail.com>
2024-10-19 00:12:28 +0900

    feat:サークルリストのstateコントローラーを作成

---
+++ lib/presentation/controller/circle_list/circle_list_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'circle_list_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$circleListControllerHash() =>
+    r'dfd34c03bc591a2d551faab2b021016902c07469';
+
+/// See also [CircleListController].
+@ProviderFor(CircleListController)
+final circleListControllerProvider = AutoDisposeAsyncNotifierProvider<
+    CircleListController, List<CircleModel>>.internal(
+  CircleListController.new,
+  name: r'circleListControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$circleListControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CircleListController = AutoDisposeAsyncNotifier<List<CircleModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
