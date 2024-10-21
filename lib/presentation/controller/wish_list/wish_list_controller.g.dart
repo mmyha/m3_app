bd8dc1f6c6040eec20eb4382ee801dd5612b4ef3
H-miura-fms <mmyha1289hm@gmail.com>
2024-10-21 11:32:18 +0900

    feat:ウィッシュリストコントローラー作成

---
+++ lib/presentation/controller/wish_list/wish_list_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'wish_list_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$wishListControllerHash() =>
+    r'8cfba47eebcc5f107e2a16debb9e411d4fa1ab65';
+
+/// See also [WishListController].
+@ProviderFor(WishListController)
+final wishListControllerProvider = AutoDisposeAsyncNotifierProvider<
+    WishListController, List<CircleWishModel>>.internal(
+  WishListController.new,
+  name: r'wishListControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$wishListControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$WishListController = AutoDisposeAsyncNotifier<List<CircleWishModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
