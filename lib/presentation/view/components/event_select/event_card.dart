d4d43e9402fcd61e8c5355e3f217782ece375e88
H-miura-fms <mmyha1289hm@gmail.com>
2024-10-21 17:31:50 +0900

    feat:イベントカードコンポーネント作成

---
+++ lib/presentation/view/components/event_select/event_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:m3_app/core/theme/m3_theme.dart';
+import 'package:m3_app/core/theme/m3_theme_config.dart';
+
+class EventCard extends StatelessWidget {
+  const EventCard({super.key, required this.eventName, required this.onTap});
+
+  final String eventName;
+  final VoidCallback onTap;
+  // TODO: イベント開催
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        color: m3ColorScheme.primaryContainer,
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Text(
+              style: TextStyle(
+                color: m3ColorScheme.surfaceTint,
+                fontSize: M3ThemeConfig().fontSize.large,
+                fontWeight: FontWeight.bold,
+              ),
+              eventName,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
