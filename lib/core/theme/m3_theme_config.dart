c52834d52e1a23e1a5fcb559922c1c8f92601a84
H-miura-fms <mmyha1289hm@gmail.com>
2024-10-21 19:18:00 +0900

    feat:themeConfigにカスタムカラーを追加

--- lib/core/theme/m3_theme_config.dart
+++ lib/core/theme/m3_theme_config.dart
@@ -1,9 +1,18 @@
+// ignore_for_file: avoid_classes_with_only_static_members
+
+import 'package:flutter/material.dart';
+
 class FontSize {
   double get small => 10;
   double get normal => 14;
   double get large => 20;
 }
 
+class M3CustomColor {
+  Color get favorite => const Color.fromARGB(255, 245, 70, 143);
+}
+
 class M3ThemeConfig {
-  FontSize get fontSize => FontSize();
+  static FontSize get fontSize => FontSize();
+  static M3CustomColor get customColor => M3CustomColor();
 }
